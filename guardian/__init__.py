f890a4a7e2ea7218dd91b759663e0721273aca71
Chukwuebuka Madu <ebukamic1111@outlook.com>
2026-02-28 23:50:25 -0400

    .

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -1,6 +1,6 @@
 from .guard_set import guard, deepguard
 from .shield import Shield
-from ._guardian_core import GuardianTypeError
+from ._guardian_core import GuardianTypeError, GuardianAccessError
 
-__all__ = ["guard", "deepguard", "Shield", "GuardianTypeError"]
-__version__ = "2.1.5"
+__all__ = ["guard", "deepguard", "Shield", "GuardianTypeError", "GuardianAccessError"]
+__version__ = "2.1.6"
