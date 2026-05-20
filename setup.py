ffb73841ee5dfeac573f9d8ccbd6ef827932898b
Michael Madu <ebukamic1111@outlook.com>
2026-05-20 12:18:19 -0300

    v3.0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ guardian_core = Extension(
 
 setup(
     name="guardian-type-enforcer",
-    version="3.0.2",
+    version="3.0.3",
     ext_modules=[guardian_core],
     packages=["guardian"],
     long_description=open("README.md", encoding="utf-8").read(),
